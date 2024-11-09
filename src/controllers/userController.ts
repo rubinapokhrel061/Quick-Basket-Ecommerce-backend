285362f6b666be0a5974271bab3b911d38d2fe7b
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2024-11-09 13:44:37 +0545

    delete user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -89,17 +89,30 @@ class AuthController {
     }
   }
 
-  public static async deleteUser(req: Request, res: Response) {
+  public static async deleteUser(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
-    const data = await User.destroy({
-      where: {
-        id,
-      },
-    });
 
-    res.status(200).json({
-      message: "User deleted Successfully",
-    });
+    try {
+      const result = await User.destroy({
+        where: { id },
+      });
+
+      if (result === 0) {
+        res.status(404).json({
+          message: `User with this id not found.`,
+        });
+        return;
+      }
+
+      res.status(200).json({
+        message: "User deleted successfully",
+      });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({
+        message: "An error occurred while trying to delete the user.",
+      });
+    }
   }
 
   public static async UpdateUser(req: Request, res: Response): Promise<void> {
